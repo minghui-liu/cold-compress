8c04dd8416b418167d9ad2334c83e8ce76a35027
Minghui Liu <minghui.liu.95@gmail.com>
2025-07-26 14:27:53 -0400

    cat all files into one command file

--- run_neurips_exps.sh
+++ run_neurips_exps.sh
@@ -2,12 +2,29 @@
 
 export OPENAI_API_KEY=
 
-python parallelize_evals.py --command_file experiments/neurips/llama_govreport.txt --num_gpus 8
-python parallelize_evals.py --command_file experiments/neurips/llama_multinews.txt --num_gpus 8
-python parallelize_evals.py --command_file experiments/neurips/qwen_gsm8k.txt --num_gpus 8
-python parallelize_evals.py --command_file experiments/neurips/qwen_medqa.txt --num_gpus 8
-python parallelize_evals.py --command_file experiments/neurips/qwen_multinews.txt --num_gpus 8
-python parallelize_evals.py --command_file experiments/neurips/qwen_govreport.txt --num_gpus 8
-python parallelize_evals.py --command_file experiments/neurips/ruler_niah131k.txt --num_gpus 8
-python parallelize_evals.py --command_file experiments/neurips/ruler_vt131k.txt --num_gpus 8
-python parallelize_evals.py --command_file experiments/neurips/ruler_cwe131k.txt --num_gpus 8
+command_files = (
+    experiments/neurips/llama_govreport.txt
+    experiments/neurips/llama_multinews.txt
+    experiments/neurips/qwen_gsm8k.txt
+    experiments/neurips/qwen_medqa.txt
+    experiments/neurips/qwen_multinews.txt
+    experiments/neurips/qwen_govreport.txt
+    experiments/neurips/ruler_niah131k.txt
+    experiments/neurips/ruler_vt131k.txt
+    experiments/neurips/ruler_cwe131k.txt
+)
+
+# cat command files into a single file called all_commands.txt
+cat "${command_files[@]}" > experiments/neurips/all_commands.txt
+# Run the parallelize_evals.py script with the combined command file
+python parallelize_evals.py --command_file experiments/neurips/all_commands.txt --num_gpus 8
+
+# python parallelize_evals.py --command_file experiments/neurips/llama_govreport.txt --num_gpus 8
+# python parallelize_evals.py --command_file experiments/neurips/llama_multinews.txt --num_gpus 8
+# python parallelize_evals.py --command_file experiments/neurips/qwen_gsm8k.txt --num_gpus 8
+# python parallelize_evals.py --command_file experiments/neurips/qwen_medqa.txt --num_gpus 8
+# python parallelize_evals.py --command_file experiments/neurips/qwen_multinews.txt --num_gpus 8
+# python parallelize_evals.py --command_file experiments/neurips/qwen_govreport.txt --num_gpus 8
+# python parallelize_evals.py --command_file experiments/neurips/ruler_niah131k.txt --num_gpus 8
+# python parallelize_evals.py --command_file experiments/neurips/ruler_vt131k.txt --num_gpus 8
+# python parallelize_evals.py --command_file experiments/neurips/ruler_cwe131k.txt --num_gpus 8
